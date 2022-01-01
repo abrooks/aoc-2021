580eea3f19fa680913166d3a8bfc7e75fe3d88c7
Aaron Brooks <aaron.brooks@viasat.com>
2021-12-31 18:32:15 -0600

    Finally have the first example working.
    Fighting with browser keybindings vs Gorilla repl.

--- aoc-2021.clj
+++ aoc-2021.clj
@@ -12,3 +12,37 @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
 ;; <=
+
+;; **
+;;; ## Day 1
+;;; 
+;;; 
+;; **
+
+;; @@
+(def d1-ex
+  [199
+   200
+   208
+   210
+   200
+   207
+   240
+   269
+   260
+   263])
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;aoc-2021/d1-ex</span>","value":"#'aoc-2021/d1-ex"}
+;; <=
+
+;; @@
+(->> d1-ex
+  (partition 2 1)
+  (map #(apply < %))
+  (filter identity)
+  count)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-unkown'>7</span>","value":"7"}
+;; <=
